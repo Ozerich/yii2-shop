beb8741b0e44bc576be046fcaf5e73fe50e24548
Vital Ozierski <v.ozierski@blak-it.com>
2019-03-24 11:25:21 +0300

    add parents to CategoryFullDTO

--- src/modules/api/models/CategoryFullDTO.php
+++ src/modules/api/models/CategoryFullDTO.php
@@ -9,6 +9,14 @@ class CategoryFullDTO extends Category implements DTO
 {
     public function toJSON()
     {
+        $parents = [];
+        $parent = $this->parent;
+
+        while ($parent) {
+            $parents[] = $parent;
+            $parent = $parent->parent;
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -19,6 +27,13 @@ class CategoryFullDTO extends Category implements DTO
             'children' => array_map(function (Category $category) {
                 return (new CategoryDTO($category))->toJSON();
             }, $this->categories),
+
+            'parents' => array_reverse(array_map(function (Category $parent) {
+                return [
+                    'id' => $parent->id,
+                    'name' => $parent->name
+                ];
+            }, $parents)),
 
             'h1_value' => empty($this->h1_value) ? $this->name : $this->h1_value,
             'seo_title' => empty($this->seo_title) ? $this->name : $this->seo_title,
