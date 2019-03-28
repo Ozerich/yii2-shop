cacc3466cd98e00a6b6158601554cc516805eac2
Vital Ozierski <v.ozierski@blak-it.com>
2019-03-28 18:14:39 +0300

    fix update price params

--- src/services/products/ProductPricesService.php
+++ src/services/products/ProductPricesService.php
@@ -3,6 +3,7 @@
 namespace ozerich\shop\services\products;
 
 use ozerich\shop\models\Product;
+use ozerich\shop\models\ProductPriceParam;
 
 class ProductPricesService
 {
@@ -15,9 +16,14 @@ class ProductPricesService
             return;
         }
 
+        $paramsCount = ProductPriceParam::find()->andWhere('product_id=:product_id', [':product_id' => $product->id])->count();
+
         $prices = $product->prices;
         $min = null;
         foreach ($prices as $price) {
+            if ($paramsCount == 2 && !$price->param_value_second_id || !$price->param_value_id) {
+                continue;
+            }
             if ($min === null || $price->value < $min) {
                 $min = $price->value;
             }
