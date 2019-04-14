a00e118135e00c52dec30efff5a06d642c7a465d
Vital Ozierski <v.ozierski@blak-it.com>
2019-04-14 15:59:33 +0300

    filter label with prefix/suffix

--- src/modules/api/models/FilterFieldDTO.php
+++ src/modules/api/models/FilterFieldDTO.php
@@ -11,7 +11,7 @@ class FilterFieldDTO extends Field implements DTO
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
+            'name' => (empty($this->value_prefix) ? '' : $this->value_prefix . ' ') . $this->name . (empty($this->value_suffix) ? '' : ', ' . $this->value_suffix),
             'type' => $this->type,
             'values' => $this->values
         ];
