61a930c7e126b95b0dfd667f4447a33a2654efde
Vital Ozierski <v.ozierski@blak-it.com>
2019-05-21 00:27:40 +0300

    add params to product short dto

--- src/modules/api/models/ProductShortDTO.php
+++ src/modules/api/models/ProductShortDTO.php
@@ -3,12 +3,33 @@
 namespace ozerich\shop\modules\api\models;
 
 use ozerich\api\interfaces\DTO;
+use ozerich\shop\constants\FieldType;
 use ozerich\shop\models\Product;
 use ozerich\shop\traits\ServicesTrait;
 
 class ProductShortDTO extends Product implements DTO
 {
     use ServicesTrait;
+
+    private function getParamsJSON()
+    {
+        $categoryFields = $this->category->categoryFields;
+
+        $field_ids = [];
+        foreach ($categoryFields as $categoryField) {
+            $field_ids[] = $categoryField->field_id;
+        }
+
+        $result = [];
+
+        foreach ($this->productFieldValues as $productFieldValue) {
+            if (in_array($productFieldValue->field_id, $field_ids)) {
+                $result[$productFieldValue->field_id] = $productFieldValue->field->type == FieldType::SELECT ? explode(';', $productFieldValue->value) : $productFieldValue->value;
+            }
+        }
+
+        return $result;
+    }
 
     public function toJSON()
     {
@@ -17,6 +38,9 @@ class ProductShortDTO extends Product implements DTO
             'url_alias' => $this->url_alias,
             'name' => $this->name,
             'label' => $this->label,
+
+            'params' => $this->getParamsJSON(),
+
             'image' => $this->image ? [
                 'small' => $this->image->getUrl('preview'),
                 'big' => $this->image->getUrl('big-preview'),
