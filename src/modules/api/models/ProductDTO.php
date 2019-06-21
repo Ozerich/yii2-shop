8368541092bd285bf60db6c5841a68dea1e37070
Vital Ozierski <v.ozierski@blak-it.com>
2019-06-21 19:26:27 +0300

    homepage

--- src/modules/api/models/ProductDTO.php
+++ src/modules/api/models/ProductDTO.php
@@ -4,9 +4,8 @@ namespace ozerich\shop\modules\api\models;
 
 use ozerich\api\interfaces\DTO;
 use ozerich\shop\constants\FieldType;
-use ozerich\shop\constants\ProductType;
+use ozerich\shop\models\Category;
 use ozerich\shop\models\Product;
-use ozerich\shop\models\ProductModule;
 use ozerich\shop\traits\ServicesTrait;
 
 class ProductDTO extends Product implements DTO
@@ -33,6 +32,37 @@ class ProductDTO extends Product implements DTO
         return $result;
     }
 
+    public function getPath()
+    {
+        $categories = [$this->category];
+
+        $max_level = null;
+        $max = null;
+
+        foreach ($categories as $category) {
+            if ($max_level === null || $category->level > $max_level) {
+                $max_level = $category->level;
+                $max = $category;
+            }
+        }
+
+        $parents = [$max];
+        $parent = $max->parent;
+
+        while ($parent) {
+            $parents[] = $parent;
+            $parent = $parent->parent;
+        }
+
+        return array_reverse(array_map(function (Category $category) {
+            return [
+                'id' => $category->id,
+                'url' => $category->getUrl(),
+                'name' => $category->name
+            ];
+        }, $parents));
+    }
+
     public function toJSON()
     {
         return [
@@ -54,8 +84,10 @@ class ProductDTO extends Product implements DTO
             'sale_disabled' => $this->sale_disabled ? true : false,
             'sale_disabled_text' => $this->sale_disabled ? $this->sale_disabled_text : null,
 
-            'params' => $this->getParamsJSON(),
             'manufacture_id' => $this->manufacture_id,
+
+            'params' => $this->getParamsJSON(),
+            'path' => $this->getPath(),
         ];
     }
 }
